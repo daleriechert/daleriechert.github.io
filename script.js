dc0e20a79b4d183030276771d29762996ac10e10
Dale Riechert <daleriechert@gmail.com>
2015-01-29 16:57:42 -0700

    additional updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var nav1, nav2, nav3, nav4, nav5;
+var nav1, nav2, nav3, nav4, nav5, nav6;
 	
 function nav1over() {
 	document.getElementById("nav1").style.backgroundColor = "#FFFFFF";
@@ -40,82 +40,118 @@ function nav5leave() {
 	document.getElementById("nav5").style.backgroundColor = "#444444";
 }
 
+function nav6over() {
+	document.getElementById("nav6").style.backgroundColor = "#FFFFFF";
+}
+function nav6leave() {
+	if (nav6 == 0)
+	document.getElementById("nav6").style.backgroundColor = "#444444";
+}
+
+
 function nav1click() {
 	document.getElementById("content-warning").style.display = "none";
 
 	nav1 = 1;
-	nav2 = 0, nav3 = 0, nav4 = 0, nav5 = 0;
+	nav2 = 0, nav3 = 0, nav4 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#FFFFFF";
 	document.getElementById("nav2").style.backgroundColor = "#444444";
 	document.getElementById("nav3").style.backgroundColor = "#444444";
 	document.getElementById("nav4").style.backgroundColor = "#444444";
 	document.getElementById("nav5").style.backgroundColor = "#444444";
+	document.getElementById("nav6").style.backgroundColor = "#444444";
 	document.getElementById("content1").style.display = "block";
 	document.getElementById("content2").style.display = "none";
 	document.getElementById("content3").style.display = "none";
 	document.getElementById("content4").style.display = "none";
 	document.getElementById("content5").style.display = "none";
+	document.getElementById("content6").style.display = "none";
 }
 
 function nav2click() {
 	nav2 = 1;
-	nav1 = 0, nav3 = 0, nav4 = 0, nav5 = 0;
+	nav1 = 0, nav3 = 0, nav4 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
 	document.getElementById("nav2").style.backgroundColor = "#FFFFFF";
 	document.getElementById("nav3").style.backgroundColor = "#444444";
 	document.getElementById("nav4").style.backgroundColor = "#444444";
 	document.getElementById("nav5").style.backgroundColor = "#444444";
+	document.getElementById("nav6").style.backgroundColor = "#444444";
 	document.getElementById("content1").style.display = "none";
 	document.getElementById("content2").style.display = "block";
 	document.getElementById("content3").style.display = "none";
 	document.getElementById("content4").style.display = "none";
 	document.getElementById("content5").style.display = "none";
+	document.getElementById("content6").style.display = "none";
 }
 
 function nav3click() {
 	nav3 = 1;
-	nav1 = 0, nav2 = 0, nav4 = 0, nav5 = 0;
+	nav1 = 0, nav2 = 0, nav4 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
 	document.getElementById("nav2").style.backgroundColor = "#444444";
 	document.getElementById("nav3").style.backgroundColor = "#FFFFFF";
 	document.getElementById("nav4").style.backgroundColor = "#444444";
 	document.getElementById("nav5").style.backgroundColor = "#444444";
+	document.getElementById("nav6").style.backgroundColor = "#444444";
 	document.getElementById("content1").style.display = "none";
 	document.getElementById("content2").style.display = "none";
 	document.getElementById("content3").style.display = "block";
 	document.getElementById("content4").style.display = "none";
 	document.getElementById("content5").style.display = "none";
+	document.getElementById("content6").style.display = "none";
 }
 
 
 function nav4click() {
 	nav4 = 1;
-	nav1 = 0, nav2 = 0, nav3 = 0, nav5 = 0;
+	nav1 = 0, nav2 = 0, nav3 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
 	document.getElementById("nav2").style.backgroundColor = "#444444";
 	document.getElementById("nav3").style.backgroundColor = "#444444";
 	document.getElementById("nav4").style.backgroundColor = "#FFFFFF";
 	document.getElementById("nav5").style.backgroundColor = "#444444";
+	document.getElementById("nav6").style.backgroundColor = "#444444";
 	document.getElementById("content1").style.display = "none";
 	document.getElementById("content2").style.display = "none";
 	document.getElementById("content3").style.display = "none";
 	document.getElementById("content4").style.display = "block";
 	document.getElementById("content5").style.display = "none";
+	document.getElementById("content6").style.display = "none";
 }
 
 function nav5click() {
 	nav5 = 1;
-	nav1 = 0, nav2 = 0, nav3 = 0, nav4 = 0;
+	nav1 = 0, nav2 = 0, nav3 = 0, nav4 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
 	document.getElementById("nav2").style.backgroundColor = "#444444";
 	document.getElementById("nav3").style.backgroundColor = "#444444";
 	document.getElementById("nav4").style.backgroundColor = "#444444";
 	document.getElementById("nav5").style.backgroundColor = "#FFFFFF";
+	document.getElementById("nav6").style.backgroundColor = "#444444";
 	document.getElementById("content1").style.display = "none";
 	document.getElementById("content2").style.display = "none";
 	document.getElementById("content3").style.display = "none";
 	document.getElementById("content4").style.display = "none";
 	document.getElementById("content5").style.display = "block";
+	document.getElementById("content6").style.display = "none";
+}
+
+function nav6click() {
+	nav6 = 1;
+	nav1 = 0, nav2 = 0, nav3 = 0, nav4 = 0, nav5 = 0;
+	document.getElementById("nav1").style.backgroundColor = "#444444";
+	document.getElementById("nav2").style.backgroundColor = "#444444";
+	document.getElementById("nav3").style.backgroundColor = "#444444";
+	document.getElementById("nav4").style.backgroundColor = "#444444";
+	document.getElementById("nav5").style.backgroundColor = "#444444";
+	document.getElementById("nav6").style.backgroundColor = "#FFFFFF";
+	document.getElementById("content1").style.display = "none";
+	document.getElementById("content2").style.display = "none";
+	document.getElementById("content3").style.display = "none";
+	document.getElementById("content4").style.display = "none";
+	document.getElementById("content5").style.display = "none";
+	document.getElementById("content6").style.display = "block";
 }
 
 function OpenTab(url) {
