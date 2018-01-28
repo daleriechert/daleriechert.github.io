8b2e8097b44d77436f1bcd74880ea57c516667aa
Dale <daleriechert@gmail.com>
2018-01-27 18:32:44 -0800

    Bush League Buds

---
+++ bushleaguebuds/instagram.js
@@ -0,0 +1,38 @@
+var userFeed = new Instafeed({
+			get: 'user',
+			userId: '6980206754',
+			clientId:'81c08da111ed4bb38d595df78f6d43b8',
+			accessToken: '6980206754.1677ed0.9a694174174e472a935741bfebd40a0c',
+			resolution: 'low_resolution',
+			limit: '9',
+			template: '<div class="col-md-4 my-4"><div class="thumbnail"><a href="{{link}}"><img style="width:100%" src="{{image}}" /></a></div><div class="caption my-2"><p>{{caption}}</p></div><div class="caption small"><p>{{model.created_time}}<br>{{location}}</p></div></div></div>',
+			filter: function(image) {
+
+				var date = new Date(image.created_time*1000);
+
+				m = date.getMonth();
+				d = date.getDate();
+				y = date.getFullYear();
+
+				var month_names = new Array ( );
+				month_names[month_names.length] = "January";
+				month_names[month_names.length] = "February";
+				month_names[month_names.length] = "March";
+				month_names[month_names.length] = "April";
+				month_names[month_names.length] = "May";
+				month_names[month_names.length] = "June";
+				month_names[month_names.length] = "July";
+				month_names[month_names.length] = "August";
+				month_names[month_names.length] = "September";
+				month_names[month_names.length] = "October";
+				month_names[month_names.length] = "November";
+				month_names[month_names.length] = "December";
+
+				var thetime = month_names[m] + ' ' + d + ', ' + y;
+
+				image.created_time = thetime;
+
+				return true;
+}
+		});
+		userFeed.run();
