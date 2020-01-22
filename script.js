d7eadc1a7e987588afac5c0c7408b57d29d3ec72
Dale <daleriechert@gmail.com>
2020-01-21 23:49:15 -0800

    Update 2020

--- script.js
+++ script.js
@@ -4,54 +4,60 @@ function nav1over() {
 	document.getElementById("nav1").style.backgroundColor = "#FFFFFF";
 }
 function nav1leave() {
-	if (nav1 == 0)
-	document.getElementById("nav1").style.backgroundColor = "#444444";
+	if (nav1 == 0) {
+		document.getElementById("nav1").style.backgroundColor = "#444444";
+	}
 }
 
 function nav2over() {
 	document.getElementById("nav2").style.backgroundColor = "#FFFFFF";
 }
 function nav2leave() {
-	if (nav2 == 0)
-	document.getElementById("nav2").style.backgroundColor = "#444444";
+	if (nav2 == 0) {
+		document.getElementById("nav2").style.backgroundColor = "#444444";
+	}
 }
 
 function nav3over() {
 	document.getElementById("nav3").style.backgroundColor = "#FFFFFF";
 }
 function nav3leave() {
-	if (nav3 == 0)
-	document.getElementById("nav3").style.backgroundColor = "#444444";
+	if (nav3 == 0) {
+		document.getElementById("nav3").style.backgroundColor = "#444444";
+	}
 }
 
 function nav4over() {
 	document.getElementById("nav4").style.backgroundColor = "#FFFFFF";
 }
 function nav4leave() {
-	if (nav4 == 0)
-	document.getElementById("nav4").style.backgroundColor = "#444444";
+	if (nav4 == 0) {
+		document.getElementById("nav4").style.backgroundColor = "#444444";
+	}
 }
 
 function nav5over() {
 	document.getElementById("nav5").style.backgroundColor = "#FFFFFF";
 }
 function nav5leave() {
-	if (nav5 == 0)
-	document.getElementById("nav5").style.backgroundColor = "#444444";
+	if (nav5 == 0) {
+		document.getElementById("nav5").style.backgroundColor = "#444444";
+	}
 }
 
 function nav6over() {
 	document.getElementById("nav6").style.backgroundColor = "#FFFFFF";
 }
 function nav6leave() {
-	if (nav6 == 0)
-	document.getElementById("nav6").style.backgroundColor = "#444444";
+	if (nav6 == 0) {
+		document.getElementById("nav6").style.backgroundColor = "#444444";
+	}
 }
 
 
 function nav1click() {
 	document.getElementById("content-warning").style.display = "none";
-
+	window.location.hash = "#home";
 	nav1 = 1;
 	nav2 = 0, nav3 = 0, nav4 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#FFFFFF";
@@ -69,6 +75,8 @@ function nav1click() {
 }
 
 function nav2click() {
+	document.getElementById("content-warning").style.display = "none";
+	window.location.hash = "#resume";
 	nav2 = 1;
 	nav1 = 0, nav3 = 0, nav4 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
@@ -86,6 +94,8 @@ function nav2click() {
 }
 
 function nav3click() {
+	document.getElementById("content-warning").style.display = "none";
+	window.location.hash = "#projects";
 	nav3 = 1;
 	nav1 = 0, nav2 = 0, nav4 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
@@ -104,6 +114,8 @@ function nav3click() {
 
 
 function nav4click() {
+	document.getElementById("content-warning").style.display = "none";
+	window.location.hash = "#design";
 	nav4 = 1;
 	nav1 = 0, nav2 = 0, nav3 = 0, nav5 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
@@ -121,6 +133,8 @@ function nav4click() {
 }
 
 function nav5click() {
+	document.getElementById("content-warning").style.display = "none";
+	window.location.hash = "#photography";
 	nav5 = 1;
 	nav1 = 0, nav2 = 0, nav3 = 0, nav4 = 0, nav6 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
@@ -138,6 +152,7 @@ function nav5click() {
 }
 
 function nav6click() {
+	document.getElementById("content-warning").style.display = "none";
 	nav6 = 1;
 	nav1 = 0, nav2 = 0, nav3 = 0, nav4 = 0, nav5 = 0;
 	document.getElementById("nav1").style.backgroundColor = "#444444";
@@ -155,10 +170,65 @@ function nav6click() {
 }
 
 function onLoad() {
-	if (window.location.hash) {        
-		nav1click(); //load content to prevent JavaScript warning
-		nav3click();
-	} else {
-    		nav1click();
+	loadFeed()
+	switch(window.location.hash) {
+		case "#home":
+			nav1click();
+			break;
+		case "#resume":
+			nav2click();
+			break;
+		case "#projects":
+			nav3click();
+			break;
+		case "#design":
+			nav4click();
+			break;
+		case "#photography":
+			nav5click();
+			break;
+		default:
+			if (window.location.hash) {        
+				nav1click(); //load content to prevent JavaScript warning
+				nav3click();
+			}
+			else {
+				nav1click();
+			}
 	}
+}
+
+function loadFeed() {
+	$.ajax({
+		url: 'https://www.flickr.com/services/feeds/photos_public.gne?id=130574536@N07&format=json',
+		type: 'GET',
+		dataType: 'jsonp',
+		jsonpCallback: 'jsonFlickrFeed',
+		success: function(result, status, xhr) {
+			$.each(result.items, function (i, item) {
+				var link = $("<a>").attr({
+					href: item.link,
+					target: "_blank"
+				})
+				var photo = $("<img>").attr({
+					src: item.media.m,
+					"class": "photo"
+				})
+				link.append(photo);
+				$("#content5").append(link)
+				if (i === 19) {
+					$("#content5").append("<br>")
+					$("#content5").append($("<a>").attr({
+						href: "https://www.flickr.com/photos/130574536@N07/",
+						target: "_blank"
+					}).text("More"))
+					return false;
+				}
+			});
+		},
+		error: function(xhr, status, error) {
+			console.log(xhr)
+			$("#content5").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+		}
+	});
 }
